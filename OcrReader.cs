08829d6a948250932fd3be42b7ac9459960ca56b
Chad Anderson <chada8898@gmail.com>
2014-03-01 12:59:31 -0600

    Added files

---
+++ OcrReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OcrReader1
+{
+    class OcrReader
+    {
+        public int ParseDigit(string ocrdigit)
+        {
+            return -1;
+        }
+    }
+}
---
+++ TestFixture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace OcrReader1
+{
+    [TestFixture]
+    public class TestFixture1
+    {
+        [Test]
+        public void TestParseDigit()
+        {
+            var ocrReader = new OcrReader();
+            string ocrDigit7 = "___"
+                             + "  |"
+                             + "  |";
+
+            Assert.AreEqual(7, ocrReader.ParseDigit(ocrDigit7));
+        }
+    }
+}
