a804f52e153c3efacb664260bc4b1b10c164ffca
Chad Anderson <chada8898@gmail.com>
2014-03-02 17:29:58 -0600

    Added file code to Program.cs.  Added README.TXT

--- Program.cs
+++ Program.cs
@@ -1,22 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Reflection;
+using System.IO;
 
 namespace OcrReader1
 {
     class Program
     {
         [STAThread]
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            const string filename = @"..\..\accounts.txt";
             string[] my_args = { Assembly.GetExecutingAssembly().Location };
 
             int returnCode = NUnit.ConsoleRunner.Runner.Main(my_args);
+            if (returnCode != 0)
+            {
+                Console.Beep();
+                Console.ReadLine();
+                return 1;
+            }
 
-            if (returnCode != 0) Console.Beep();
+            // Open file & read lines into strings
+            try
+            {
+                using (StreamReader reader = File.OpenText(filename))
+                {
+                    string buffer = null;
+                    var ocrReader = new OcrReader();
+
+                    do
+                    {
+                        string ocrstring = "";
+
+                        for (int index = 0; index < 3; index++)
+                        {
+                            if ((buffer = reader.ReadLine()) != null)
+                            {
+                                ocrstring += buffer.Substring(0, 27);
+                            }
+                            else break;
+                        }
+
+                        if (buffer != null)
+                        {
+                            buffer = reader.ReadLine();
+                            int accountNumber = ocrReader.ParseString(ocrstring);
+                            Console.WriteLine(accountNumber);
+                        }
+
+                    } while (buffer != null);
+                }
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Error reading file " + filename);
+            }
+
             Console.ReadLine();
+            return 0;
         }
     }
 }
